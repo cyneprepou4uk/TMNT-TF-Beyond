a2ae897407168795d2d2421e83563e9ead9aaeea
cyneprepou4uk <cyneprepou4uk@gmail.com>
2021-01-01 22:45:26 +0300

    мусор

--- bank_0D.asm
+++ bank_0D.asm
@@ -1966,17 +1966,6 @@ _off0118C43_0A:
 - D 0 - I - 0x034C55 0D:8C45: FF        .byte $FF   ; 
 
 
-; bzk мусор
-- - - - - - 0x034C56 0D:8C46: FD        .byte $FD   ; 
-- - - - - - 0x034C57 0D:8C47: 06        .byte $06   ; 
-- - - - - - 0x034C58 0D:8C48: F4        .byte $F4   ; 
-- - - - - - 0x034C59 0D:8C49: FD        .byte $FD   ; 
-- - - - - - 0x034C5A 0D:8C4A: 04        .byte $04   ; 
-- - - - - - 0x034C5B 0D:8C4B: 04        .byte $04   ; 
-- - - - - - 0x034C5C 0D:8C4C: 40        .byte $40   ; 
-- - - - - - 0x034C5D 0D:8C4D: FF        .byte $FF   ; 
-
-
 
 _off0118C4E_07:
 - D 0 - I - 0x034C5E 0D:8C4E: DE        .byte $DE   ; 
